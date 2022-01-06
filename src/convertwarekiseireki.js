53f47c394526ac48cfed5ac8fd229bb6a2f89c91
YMW <youmw99@gmail.com>
2022-01-06 22:56:17 +0900

    function modified(which can show the convertwarekiseireki.js results)

--- src/convertwarekiseireki.js
+++ src/convertwarekiseireki.js
@@ -1,3 +1,9 @@
+//다음 사항에 유의하여 코드 작성필요
+//1. convertWarekiToSeireki의 경우
+//1-1. 연호와 숫자를 구분할 수 있는 방법
+//2. convertSeirekiToWareki의 경우
+//3. 공통
+//3-1. 잘못들어오는 값에 대한 처리(ex:-1111,Z2324,String등)
 function convertWarekiToSeireki(str, number){
 
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,27 @@
 function execWarekiToSeireki(){
-
+    commonExecfunc('warekiInput','seirekiOutput');
 }
 
 function execSeirekiToWareki(){
-    
+    commonExecfunc('seirekiInput','warekiOutput');
+}
+
+function commonExecfunc(inputId,outputId){
+    var input = document.getElementById(inputId).value;
+
+    var judgeWarekiSeireki = function(){
+            if(inputId==='warekiInput'){
+                return convertWarekiToSeireki(input);
+            }
+            else if(inputId==='seirekiInput'){
+                return convertSeirekiToWareki(input)
+            }
+            else{
+                return null;
+            }
+        }
+
+    var result = judgeWarekiSeireki();
+    var output = document.getElementById(outputId);
+    output.value = result;
 }
